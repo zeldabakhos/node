8ca080f1c2a10da7473d4a65d9a1c53cb1b8bffb
zeldabakhos <zelda.bakhos@epita.fr>
2024-10-07 14:51:11 +0200

    test

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 console.log("test")
+
+
+// object
+
+const courses = []
+
+const student = {
+    firstName: "Marie",
+    age: 20,
+    courses
+}
+
+student.age = 21
+student.grade = "A"
+
+courses.push("Math", "Physics", "Chemistry")
+
+const foundCourse = courses.indexOf("Physics")
+
+const firstCourse = courses.slice(0,2)
+
+console.log(student)
+console.log(foundCourse) 
+console.log(firstCourse)
